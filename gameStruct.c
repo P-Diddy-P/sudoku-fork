08ed1f1d4c471c1566cc23eea15f967f1184efd9
P-Diddy-P <omer.sh10@gmail.com>
2019-08-04 13:14:18 +0300

    Added board validation to gameStruct
    Note that printBoard still doesn't consider game flags. To be updated when game states are decided.

--- gameStruct.c
+++ gameStruct.c
@@ -1,6 +1,10 @@
 #include "gameStruct.h"
 
+
+/***********************************************************************/
 /* TODO PRIVATE FUNCTIONS - SHOULD NOT BE INCLUDED IN HEADER FILE TODO */
+/***********************************************************************/
+
 
 void printDashes(int n) {
 	int i;
@@ -10,6 +14,7 @@ void printDashes(int n) {
 	}
 	putchar('\n');
 }
+
 
 char getControlChar(game *gptr, int row, int col) {
 	if (gptr->flag[row][col] == FIXED) { /* TODO add game mode to condition? */
@@ -21,22 +26,101 @@ char getControlChar(game *gptr, int row, int col) {
 	return ' ';
 }
 
+
+void checkRowForErrors(game *gptr, int **errorBoard, int rowId) {
+	int j; /* iteration variable for current checked cell. */
+	int k; /* iteration variable for j subsequent comparisons */
+
+	for (j=0; j<gptr->sideLength; j++) {
+		if (gptr->user[rowId][j] == 0) {
+			continue; /* skip 0 values for performance, those don't cause errors */
+		}
+
+		for (k=j+1; k<gptr->sideLength; k++) {
+			if (gptr->user[rowId][j] == gptr->user[rowId][k]) {
+
+				errorBoard[rowId][j] = ERROR;
+				errorBoard[rowId][k] = ERROR;
+			}
+		}
+	}
+}
+
+
+void checkColumnForErrors(game *gptr, int **errorBoard, int colId) {
+	int i; /* iteration variable for current checked cell. */
+	int k; /* iteration variable for i subsequent comparisons */
+
+	for (i=0; i<gptr->sideLength; i++) {
+		if (gptr->user[i][colId] == 0) {
+			continue; /* skip 0 values for performance, those don't cause errors */
+		}
+
+		for (k=i+1; k<gptr->sideLength; k++) {
+			if (gptr->user[i][colId] == gptr->user[k][colId]) {
+
+				errorBoard[i][colId] = ERROR;
+				errorBoard[k][colId] = ERROR;
+			}
+		}
+	}
+}
+
+
+void checkBlockForErrors(game *gptr, int **errorBoard, int blockId) {
+	int si = blockId - (blockId % gptr->rows); /* row of first cell in block */
+	int sj = (blockId % gptr->rows) * gptr->cols; /* column of first cell in block */
+	int ci, cj;
+	int ri, rj;
+
+	for (ci=si; ci<si+gptr->rows; ci++) { /* TODO extremely convoluted iteration, consider revising */
+		for (cj=sj; cj<sj+gptr->cols; cj++) {
+			printf("[[testing cell[%d][%d]=%d]]\n\n", ci, cj, gptr->user[ci][cj]);
+
+			if (gptr->user[ci][cj] == 0) {
+				printf("Value is 0, skipping comparisons.\n\n");
+				continue; /* skip 0 values for performance, those don't cause errors */
+			}
+
+			for (ri = ci + (cj + 1)/gptr->cols; ri<si+gptr->rows; ri++) {
+				rj = (ri == ci) ? (cj + 1) % gptr->cols : sj;
+
+				for (/* defined above */; rj<sj+gptr->cols; rj++) {
+					printf("    comparing [%d][%d]=(%d) ??? [%d][%d]=(%d)", ci, cj, gptr->user[ci][cj], ri, rj, gptr->user[ri][rj]);
+					if (gptr->user[ci][cj] == gptr->user[ri][rj]) {
+						printf(": numbers match - marking errors.");
+						errorBoard[ci][cj] = ERROR;
+						errorBoard[ri][rj] = ERROR;
+					}
+					printf("\n");
+				}
+			}
+			printf("\n");
+		}
+	}
+}
+
+
+/*******************************************************/
 /* TODO PUBLIC FUNCTIONS - INCLUDE IN HEADER FILE TODO */
+/*******************************************************/
+
 
 void initBoard(game * gptr, int rows, int cols) {
 	/* Construct a game board with given rows and cols per block */
-	int i, sideLength = rows*cols;
+	int i;
 
 	gptr->cols = cols;
 	gptr->rows = rows;
+	gptr->sideLength = rows * cols;
 
-	gptr->user = malloc(sideLength * sizeof(int *));
-	gptr->flag = malloc(sideLength * sizeof(int *));
+	gptr->user = malloc(gptr->sideLength * sizeof(int *));
+	gptr->flag = malloc(gptr->sideLength * sizeof(int *));
 
-	for (i=0; i<sideLength; i++) {
-		gptr->user[i] = malloc(sideLength * sizeof(int));
-		gptr->flag[i] = malloc(sideLength * sizeof(int));
-	} /* TODO currently calloc returns null pointer for columns. Check why */
+	for (i=0; i<gptr->sideLength; i++) {
+		gptr->user[i] = calloc(gptr->sideLength, sizeof(int));
+		gptr->flag[i] = calloc(gptr->sideLength, sizeof(int));
+	}
 
 	/* compound statement check for memory allocation errors */
 	/* TODO: refactor to error functions in the compound statement, reduce clutter */
@@ -49,7 +133,7 @@ void initBoard(game * gptr, int rows, int cols) {
 			printf("Row pointer memory allocation failed, got null pointer.\n");
 			exit(1);
 		}
-		for (i=0; i<sideLength; i++) {
+		for (i=0; i<gptr->sideLength; i++) {
 			if (gptr->user[i] == NULL || gptr->flag[i] == NULL) {
 				printf("Column pointer memory allocation failed, got null pointer.\n");
 				exit(1);
@@ -60,17 +144,15 @@ void initBoard(game * gptr, int rows, int cols) {
 }
 
 void printBoard(game *gptr) {
-	printf("Starting printboard\n");
-	int sideLength = gptr->rows * gptr->cols;
-	int dashLength = 4*sideLength + gptr->rows + 1;
+	int dashLength = 4*gptr->sideLength + gptr->rows + 1;
 	int i, j; /* iteration variables: i for current row, j for current column */
 
-	for (i=0; i<sideLength; i++) {
+	for (i=0; i<gptr->sideLength; i++) {
 		if (i % gptr->rows == 0) {
 			printDashes(dashLength);
 		}
 
-		for (j=0; j<sideLength; j++) {
+		for (j=0; j<gptr->sideLength; j++) {
 			if (j % gptr->cols == 0) {
 				putchar('|');
 			}
@@ -79,4 +161,50 @@ void printBoard(game *gptr) {
 		printf("|\n");
 	}
 	printDashes(dashLength);
+}
+
+void updateBoard(game *gptr, int len, int *rowIds, int *colIds, int *values, int * flags) {
+	int addFlags = !(flags == NULL);
+	int r, c, i;
+
+	for (i=0; i<len; i++) {
+		r = rowIds[i]; c = colIds[i];
+		gptr->user[r][c] = values[i];
+		if (addFlags) {
+			gptr->flag[r][c] = flags[i];
+		}
+	}
+}
+
+void updateRandom(game *gptr, int seed, int addFlags) {
+	srand(seed);
+	int i, j;
+
+	for (i=0; i<gptr->sideLength; i++) {
+		for (j=0; j<gptr->sideLength; j++) {
+			gptr->user[i][j] = rand() % gptr->sideLength + 1;
+			gptr->flag[i][j] = (rand() % 2)*addFlags;
+		}
+	}
+}
+
+int checkForErrors(game *gptr) {
+	int i, j, errorsInBoard = 0;
+	int **validationBoard = malloc(gptr->sideLength * sizeof(int *));
+	for (i=0; i<gptr->sideLength; i++) {
+		validationBoard[i] = calloc(gptr->sideLength, sizeof(int *));
+	}
+
+	for (i=0; i<gptr->sideLength; i++) {
+		checkRowForErrors(gptr, validationBoard, i);
+		checkColumnForErrors(gptr, validationBoard, i);
+		checkBlockForErrors(gptr, validationBoard, i);
+	}
+	for (i=0; i<gptr->sideLength; i++) {
+		for (j=0; j<gptr->sideLength; j++) {
+			gptr->flag[i][j] = (gptr->flag[i][j] == FIXED) ? FIXED : validationBoard[i][j];
+			errorsInBoard = (errorsInBoard) ? errorsInBoard : validationBoard[i][j];
+		}
+	}
+	return errorsInBoard;
 }
--- gameStruct.h
+++ gameStruct.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 /* Constant identifiers for fixed values and error values in the board */
+#define					VALID  0
 #define 				FIXED  1
 #define					ERROR  2
 
@@ -14,12 +15,16 @@
  * 2 columns. in order to keep a sudoku like structure, the blocks are ordered
  * in 3 columns and 2 rows, leading to a 6-by-6 game board.
  *
+ * sideLength indicates the number of cells in each row, column and block, and is
+ * mostly added for convenience purposes.
+ *
  * ^(TODO consider changing member name to reflect on it's block nature)
  * */
 typedef struct game {
 
 	int rows;
 	int cols;
+	int sideLength;
 	int **user;
 	int **flag;
 }game;
@@ -31,3 +36,18 @@ void initBoard(game * gptr, int rows, int cols);
 
 void printBoard(game *gptr);
 /* Prints the game board, including error marks and fixed values */
+
+void updateBoard(game *gptr, int len, int *rowIds, int *colIds, int *values, int * flags);
+/* Inserts values from array to the board for debug purposes. a flags array doesn't have to
+ * be provided (can be replaced by null pointer) */
+
+void updateRandom(game *gptr, int seed, int addFlags);
+/* Fills the board entirely with random values according to a predefined seed. Can also fill
+ * flags randomly. NOTE: addFlags must be binary (0 or 1)! */
+
+int checkForErrors(game *gptr);
+/* Checks the entire board for errors in row, column and block and returns 0 for no errors, 1 otherwise.
+ * Function also updates non-fixed cells to have an error flag if they have a duplicate value, and removes
+ * error flags for cells with no value or cells with no duplicate value in their row.
+ *
+ * checkForErrors replaces the board entirely with valid/error values, regardless of old validity values. */
